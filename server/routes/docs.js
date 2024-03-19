e528461d9c5b9d936b5f128127700b813853336f
John Pieri <jvpieri@gmail.com>
2024-03-19 15:30:11 -0400

    Added comments.

--- server/routes/docs.js
+++ server/routes/docs.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+
+//This is a route handler for get requests
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../views/docs.html'));
 });
 
 module.exports = router;
+
+
+// ? Other route handlers: router.post(), router.put(), router.delete(),
+
+// ? The path.join(__dirname, '../views/docs.html') part constructs the absolute path to the docs.html file
+
+// ? __dirname represents the directory of the current module (docs.js), and ../views/docs.html is the relative path to the docs.html file from the docs.js file.
