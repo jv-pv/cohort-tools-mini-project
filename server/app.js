5df25abde13b62e4940f0da3bdaf8343d36a3fbb
John Pieri <jvpieri@gmail.com>
2024-03-19 15:29:58 -0400

    Added comments.

--- server/app.js
+++ server/app.js
@@ -4,31 +4,46 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
+// Importing route modules
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require('./routes/auth')
 var apiRouter = require('./routes/api')
 var docsRouter = require('./routes/docs');
 
+// Create a new express instance
 var app = express();
 
+
+// Set up and mount middleware - processes the request before reaching the final response handlers
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Serve static files
 app.use(express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// Configure CORS rules (Cross Origin Resourse Sharing)
 app.use(
   cors({
     origin: [process.env.REACT_APP_URI],
   })
 );
 
+
+// Mount the differnt routes to handle the specified request enpoint
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use('/auth', authRouter);
-app.use('/api', apiRouter)
+app.use('/api', apiRouter);
 app.use('/docs', docsRouter);
 
 module.exports = app;
+
+
+// ? Middleware
+/** 
+ Think of middleware as a series of steps or checkpoints that the request goes through before reaching its final destination. Each middleware function has access to the request object, the response object, and the next middleware function in the application's request-response cycle.
+ */
