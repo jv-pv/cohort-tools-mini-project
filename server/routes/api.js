d38241f64767644e927992d93189323eecd905c8
John Pieri <jvpieri@gmail.com>
2024-03-19 15:30:41 -0400

    Added routing path to handle /api endpoint.

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,8 +5,8 @@ const cohorts = require("../cohorts.json")
 const students = require("../students.json");
 
 /* GET home page. */
-router.get('/api', function(req, res, next) {
-  res.sendFile(patch)
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/api.html"))
 })
 
 router.get('/cohorts', function(req, res, next) {
