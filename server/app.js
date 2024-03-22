db022f9c48a580662a4f7b10a709211ca61c2b75
John Pieri <jvpieri@gmail.com>
2024-03-22 10:21:00 -0400

    Moved mongoose  connection to bottom

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -8,20 +8,12 @@ var cors = require("cors");
 // Importing route modules
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var authRouter = require('./routes/auth')
-var apiRouter = require('./routes/api')
-var docsRouter = require('./routes/docs');
+var authRouter = require("./routes/auth");
+var apiRouter = require("./routes/api");
+var docsRouter = require("./routes/docs");
 
 // Create a new express instance
 var app = express();
-
-// Connect to MongoDB database
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
-  .then(x => console.log(`Connected to Database: "${x.connection[0].name}"`))
-  .catch(err => console.error("Error connecting to MongoDB", err));
-
 
 // Set up and mount middleware - processes the request before reaching the final response handlers
 app.use(logger("dev"));
@@ -32,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 // Configure CORS rules (Cross Origin Resourse Sharing)
 app.use(
   cors({
@@ -40,18 +31,21 @@ app.use(
   })
 );
 
-
 // Mount the differnt routes to handle the specified request enpoint
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use('/auth', authRouter);
-app.use('/api', apiRouter);
-app.use('/docs', docsRouter);
+app.use("/auth", authRouter);
+app.use("/api", apiRouter);
+app.use("/docs", docsRouter);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 module.exports = app;
 
-
 // ? Middleware
 /** 
- Think of middleware as a series of steps or checkpoints that the request goes through before reaching its final destination. Each middleware function has access to the request object, the response object, and the next middleware function in the application's request-response cycle.
- */
+   Think of middleware as a series of steps or checkpoints that the request goes through before reaching its final destination. Each middleware function has access to the request object, the response object, and the next middleware function in the application's request-response cycle.
+   */
