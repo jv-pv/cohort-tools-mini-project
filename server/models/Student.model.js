85cfa26cd15f86e5ef218c67389e23378f177773
John Pieri <jvpieri@gmail.com>
2024-03-22 10:21:35 -0400

    Edited schema phone type from number to string.

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema
 const studentSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: { type: String, unique: true },
-    phone: { type: Number, maxlength: 16, unique: true },
+    email: { type: String, unique: true},
+    phone: { type: String, maxlength: 16, unique: true },
     linkedinUrl: String,
     languages: Array,
     program: String,
     background: String,
     image: String,
-    cohort: Number,
+    cohort: {type: Schema.Types.ObjectId, ref: "Cohort"},
     projects: Array
 })
 
